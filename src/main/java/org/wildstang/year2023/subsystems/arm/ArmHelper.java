1fa2a97247718c2aa8aeb1b384c27a08a9017a0f
jwannebo3524 <jwannebo3524@stu.d214.org>
2023-01-23 20:05:08 -0600

    updates

--- src/main/java/org/wildstang/year2023/subsystems/arm/ArmHelper.java
+++ src/main/java/org/wildstang/year2023/subsystems/arm/ArmHelper.java
@@ -7,8 +7,10 @@ import org.wildstang.hardware.roborio.outputs.WsPhoenix;
 import org.wildstang.year2023.robot.WSInputs;
 import org.wildstang.year2023.robot.WSOutputs;
 import java.util.Random;
-import org.javatuples.Quartet;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Arrays;
 /**
  * Sample Subsystem that controls a motor with a joystick.
  * @author Liam
@@ -21,41 +23,36 @@ public class ArmHelper implements Subsystem {
 
     // states
     //double speed;
-    int InSize = 3;
-    int MidSize = 6;
     int OutSize = 3;
+    int MidSize = 6;
+    int InSize = 3;
     double[][] W1;
     double[][] W2;
     double[] B1;
     double[] B2;
 
-    String file = "NetworkData.txt"
+    String fileName = "NetworkData.txt";
 
     @Override
-    public void init() {
-
+    public void init(){
+        
     }
 
     @Override
     public void resetState() {
-        speed = 0;
     }
 
     @Override
     public void update() {
-        motor.setValue(speed);
     }
 
     @Override
     public void inputUpdate(Input source) {
-        if (source == joystick) {
-            speed = joystick.getValue();
-        }
     }
 
     @Override
     public String getName() {
-        return "Sample";
+        return "ArmHelper";
     }
 
     @Override
@@ -63,17 +60,13 @@ public class ArmHelper implements Subsystem {
     }
     private void initNetwork(){
         W1 = initWeights(InSize,MidSize);
-        B1 = initWeights(MidSize);
+        B1 = initBias(MidSize);
         W2 = initWeights(MidSize,OutSize);
-        B2 = initWeights(OutSize);
+        B2 = initBias(OutSize);
     }
-    private void saveToFile(){
-        File myObj = new File(file);
-        Quad data = new Quartet<double[][],double[],double[][],double[]>(W1,B1,W2,B2);
 
-    }
-    private double[][] initWeights(inSize,outSize){
-        double[outSize][inSize] out;
+    private double[][] initWeights(int inSize,int outSize){
+        double[][] out = new double[outSize][inSize];
         Random random = new Random();
         int c = 0;
         while(c<outSize){
@@ -86,8 +79,8 @@ public class ArmHelper implements Subsystem {
         }
         return out;
     }
-    private double[] initBias(outSize){
-        double[outSize] out;
+    private double[] initBias(int outSize){
+        double[] out = new double[outSize];
         Random random = new Random();
         int c = 0;
         while(c<outSize){
@@ -95,5 +88,77 @@ public class ArmHelper implements Subsystem {
             c += 1;
         }
         return out;
+    }
+    private void printIn() {
+        File file = new File(fileName);
+        boolean check = file.createNewFile();
+        if (check) {
+            initNetwork();//activates if the file is successful in being created 
+        } else {
+            //activates if one was already there with that name
+        }
+        FileReader input = new FileReader(file);
+    }
+    private void printOut() {
+        File file = new File(fileName);
+        FileWriter output = new FileWriter(file,false);
+        double [][][] tempData = {W1,{B1},W2,{B2}};
+        output.write(Arrays.toString(tempData));
+    }
+    private void parseData(String dataString){
+        String[] data = dataString.split(""); 
+        int c = 0;
+        int part = 0;
+        int partOfPart = 0;
+        int partOfPartOfPart = 0;
+        int partOfPartOfPartOfPart = 0;
+        int layer = 0;
+        int pos1 = 0;
+        int pos2 = 0;
+        while(c<data.length){
+            if (data[c] == "[") {
+                pos2 = c-1;
+                layer++;
+                if (layer == 2) {
+                    partOfPart = -1;
+                    part++;
+                } else if (layer == 3) {
+                    partOfPart++;
+                    partOfPartOfPart = -1;
+                }
+            } else if (data[c] == "]") {
+                layer--;
+                if (layer == 2) {
+                pos1 = pos2;
+                pos2 = c;
+                partOfPartOfPart++;
+                if (part == 1) {
+                W1[partOfPart][partOfPartOfPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            } else if (part == 2) {
+                B1[partOfPartofPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            } else if (part == 3) {
+                W2[partOfPart][partOfPartOfPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            } else {
+                B2[partOfPartOfPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            }
+                }
+            }
+            if (data[c] == "," && layer == 3) {
+                pos1 = pos2;
+                pos2 = c;
+                partOfPartOfPart++;
+                if (part == 1) {
+                String[] um = String.join("",Arrays.copyOfRange(data,pos1 + 2, pos2));
+                W1[partOfPart][partOfPartOfPart] = Double.parseDouble(um);
+            } else if (part == 2) {
+                B1[partOfPartOfPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            } else if (part == 3) {
+                W2[partOfPart][partOfPartOfPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            } else {
+                B2[partOfPartOfPart] = Double.parseDouble(data.substring(pos1 + 2, pos2));
+            }
+            }
+            c++;
+        }
     }
 }
--- src/main/java/org/wildstang/year2023/subsystems/arm/Network.java
+++ src/main/java/org/wildstang/year2023/subsystems/arm/Network.java
@@ -1,26 +1,25 @@
 package org.wildstang.year2023.subsystems.arm;
-import org.javatuples.Triplet;
-
+//import java.io.math;
 public class Network { 
     private double a = 1.0;
-    private double[] dot(double[] a, double[] b){
+    private double dot(double[] a, double[] b){
         int c = 0;
         double out = 0;
         while(c<a.length){
             out += a[c]*b[c];
             c += 1;
         }
-        return out
+        return out;
     }
     private double neuronForward(double[] weights,double bias, double[] inp){ //using ReLu activation function (b/c its easy. Idk if the performance is actually any good)
-        double out = Network.dot(weights,inp)+bias;
+        double out = dot(weights,inp)+bias;
         if(out<0){
-            out = a*math.exp(z)-1;
+            out = a*Math.exp(out)-1;
         }
-        return out
+        return out;
     }
-    private Triplet<double[],double[],double> neuronBackward(double[] weights,double bias, double[] inp, double DeDo,double learn){
-        z = neuronForward(weights,bias,inp);
+    private double[][] neuronBackward(double[] weights,double bias, double[] inp, double DeDo,double learn){
+        double z = neuronForward(weights,bias,inp);
         if(z>0){
             DeDo = DeDo;
         }
@@ -28,28 +27,41 @@ public class Network {
             DeDo = DeDo*(1+z);
         }
         int c = 0;
-        double[weights.length] DeDi;
+        double[] DeDi = new double[weights.length];
         while(c<weights.length){
             DeDi[c] = (DeDo*weights[c]);
             weights[c] = weights[c]-(learn*DeDo*inp[c]);
             c += 1;
         }
         bias = bias - (learn*DeDo);
-        return new Triplet<double[],double[],double>(DeDi,weights,bias)
+        double[][] rData = {DeDi,weights,{bias}};
+        return rData;
     }
     
-    private Triplet<double[],double[][],double[]> layerBackward(double[][] weights, double[] bias, double[] inp, double[] DeDo, double learn){
+    private double[][][] layerBackward(double[][] weights, double[] bias, double[] inp, double[] DeDo, double learn){
         int c = 0;
-        double[inp.length] out;
+        double[] out = new double[inp.length];
         while(c<weights.length){
-            double[inp.length] stuff;
-            stuff,weights[c],bias[c] = neuronBackward(weights[c],bias[c],inp,DeDo[c],learn);
+            double[] stuff = new double[inp.length];
+            double[][] ugh = neuronBackward(weights[c],bias[c],inp,DeDo[c],learn);
+            stuff = ugh[0];
+            weights[c] = ugh[1];
+            bias[c] = ugh[2][0];
             int c2 = 0;
-            while(c2<stuff.length):
+            while(c2<stuff.length){
                 out[c] += stuff[c];
                 c2 += 1;
+            }
             c += 1;
         }
-        return new Triplet<double[],double[][],double[]>(out,weights,bias)
+        double[][][] rData = {{out},weights,{bias}};
+        return rData;
+    }
+    private double[] layerForward(double[][] weights,double[] bias, double[] inp){
+        double[] out = new double[weights.length];
+        int c = 0;
+        while(c<weights.length){
+            out[c] = neuronForward(weights[c],bias[c],inp);
+        }
     }
 }
