889522812baba3675a2845bc2c73fee773733918
jwannebo3524 <jwannebo3524@stu.d214.org>
2023-01-26 18:29:32 -0600

    updates

--- src/main/java/org/wildstang/year2023/subsystems/arm/wrist.java
+++ src/main/java/org/wildstang/year2023/subsystems/arm/wrist.java
@@ -9,6 +9,10 @@ import org.wildstang.year2023.robot.WSOutputs;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.wildstang.hardware.roborio.outputs.WsSparkMax;
 import org.wildstang.framework.core.Core;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+
+import org.wildstang.year2023.subsystems.arm.ArmConstants;
 /**
  * Sample Subsystem that controls a motor with a joystick.
  * @author Liam
@@ -23,17 +27,25 @@ public class wrist {
     //double speed;
     //private DigitalInput Rotate_Clockwise, Rotate_Counter_Clockwise;
     private WsSparkMax BaseMotor;
+    private AbsoluteEncoder Encoder;
     //private int direction;
     //private double BaseSpeed = 5.5;
    // private double EncodedPositition;
-    private double Pos;
-    private static final double Tolerance = 1;
+    private double position;
+    private static final double tolerance = 1;
     private static final double minPosition = 0;
     private static final double maxPosition = 0;
     public void init() {
         //joystick = (WsJoystickAxis) WSInputs.DRIVER_LEFT_JOYSTICK_Y.get();
-        Pos = minPosition;
+        position = minPosition;
         BaseMotor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.WRIST);
+        Encoder = BaseMotor.getController().getAbsoluteEncoder(Type.kDutyCycle); //idk if this is actually right
+        Encoder.setInverted(false); //or this stuff
+        Encoder.setPositionConversionFactor(360.0);
+        Encoder.setVelocityConversionFactor(360.0/60.0);
+        BaseMotor.initClosedLoop(ArmConstants.WRIST_P, ArmConstants.WRIST_I, ArmConstants.WRIST_D,0, this.Encoder);
+        BaseMotor.setCurrentLimit(ArmConstants.WRIST_CURRENT_LIMIT, ArmConstants.WRIST_CURRENT_LIMIT, 0);
+        resetState();
     }
 
     public void stopMotor() {
@@ -43,7 +55,7 @@ public class wrist {
     public void goToPosition(double pos) {
         if (pos > minPosition && pos < maxPosition) {
             BaseMotor.setPosition(pos);
-            Pos = pos;
+            position = pos;
             SmartDashboard.putNumber("Wrist target", pos);
         } else {
             SmartDashboard.putNumber("Wrist target", -999);
@@ -55,26 +67,20 @@ public class wrist {
     }
     
     public boolean isReady(){
-        SmartDashboard.putNumber("Wrist pos", Pos);
-        if(Math.abs(BaseMotor.getPosition()-Pos)<Tolerance){
+        SmartDashboard.putNumber("Wrist pos", position);
+        if(Math.abs(BaseMotor.getPosition()-position)<tolerance){
             return true;
         }
         return false;
     }
 
     public void resetState() {
-        Pos = minPosition;
+        position = minPosition;
+        BaseMotor.setPosition(position);
     }
-
-    public void update() {
-    
-    }
-
 
     public String getName() {
         return "Wrist";
     }
 
-    public void selfTest() {
-    }
 }
