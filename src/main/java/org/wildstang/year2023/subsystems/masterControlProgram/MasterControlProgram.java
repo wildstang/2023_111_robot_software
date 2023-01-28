bf9ea3bfb35adbcae51c0c7b4cc4493d2fb3fb6b
jwannebo3524 <jwannebo3524@stu.d214.org>
2023-01-28 13:23:59 -0600

    Update MasterControlProgram.java

--- src/main/java/org/wildstang/year2023/subsystems/masterControlProgram/MasterControlProgram.java
+++ src/main/java/org/wildstang/year2023/subsystems/masterControlProgram/MasterControlProgram.java
@@ -12,12 +12,12 @@ import org.wildstang.year2023.subsystems.arm.arm;
 import org.wildstang.year2023.subsystems.arm.lift;
 import org.wildstang.year2023.subsystems.arm.wrist;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import java.util.*;
 
 public class MasterControlProgram implements Subsystem {
     
     //inputs
-    private DigitalInput highGoal, midGoal, lowGoal, station, cubeMode, coneMode,forward,reverse,reset,halt;
+    private DigitalInput highGoal, midGoal, lowGoal, stationForward, stationReverse, cubeMode, coneMode,groundForward,groundReverse,reset,halt;
     private AnalogInput liftManual;
     // motors
     
@@ -40,30 +40,8 @@ public class MasterControlProgram implements Subsystem {
     private static final double liftDeadband = 0.05;
     private static final double liftSpeedFactor = 0.5;
     private static final double liftResetBound = -0.9;
-    // states
-    private enum positions{
-        HOLDING(0,0,0,"HOLDING"),
-        GROUND_FORWARD(0,0,0,"GROUND_FORWARD"),
-        GROUND_REVERSE(0,0,0,"GROUND_REVERSE"),
-        CONE_MID_FORWARD(0,0,0,"CONE_MID_FORWARD"),
-        CONE_MID_REVERSE(0,0,0,"CONE_MID_REVERSE"),
-        CONE_HIGH_FORWARD(0,0,0,"CONE_HIGH_FORWARD"),
-        CUBE_MID_FORWARD(0,0,0,"CUBE_MID_FORWARD"),
-        CUBE_MID_REVERSE(0,0,0,"CUBE_MID_REVERSE"),
-        CUBE_HIGH_FORWARD(0,0,0,"CUBE_HIGH_FORWARD"),
-        STATION_FORWARD(0,0,0,"STATION_FORWARD"),
-        STATION_REVERSE(0,0,0,"STATION_REVERSE");
-        public final double lpos;
-        public final double apos;
-        public final double wpos;
-        public final String name;
-        private positions(double liftPos, double armPos, double wristPos,String Name){
-            this.lpos = liftPos;
-            this.apos = armPos;
-            this.wpos = wristPos;
-            this.name = Name;
-        }
-    }
+
+    private static final double liftFlipPos = 0;
 
     private enum modes{
         CONE,
@@ -71,12 +49,52 @@ public class MasterControlProgram implements Subsystem {
         FORWARD,
         REVERSE,
         LIFT_MANUAL,
-        LIFT_AUTOMATIC;
+        LIFT_AUTOMATIC,
+        HOLDING_ARM,
+        HOLDING_LIFT,
+        FREE;
     } //true = cone, false = cube
+
+    // states
+    private enum positions{
+        HOLDING(0,0,0,"HOLDING",modes.FORWARD),
+        GROUND_FORWARD(0,0,0,"GROUND_FORWARD",modes.FORWARD),
+        GROUND_REVERSE(0,0,0,"GROUND_REVERSE",modes.REVERSE),
+
+        CONE_LOW_FORWARD(0,0,0,"CONE_LOW_FORWARD",modes.FORWARD),
+        CONE_MID_FORWARD(0,0,0,"CONE_MID_FORWARD",modes.FORWARD),
+        //CONE_MID_REVERSE(0,0,0,"CONE_MID_REVERSE"),
+        CONE_HIGH_FORWARD(0,0,0,"CONE_HIGH_FORWARD",modes.FORWARD),
+
+        CUBE_LOW_FORWARD(0,0,0,"CUBE_LOW_FORWARD",modes.FORWARD),
+        CUBE_MID_FORWARD(0,0,0,"CUBE_MID_FORWARD",modes.FORWARD),
+        //CUBE_MID_REVERSE(0,0,0,"CUBE_MID_REVERSE"),
+        CUBE_HIGH_FORWARD(0,0,0,"CUBE_HIGH_FORWARD",modes.FORWARD),
+
+        STATION_FORWARD(0,0,0,"STATION_FORWARD",modes.FORWARD),
+        STATION_REVERSE(0,0,0,"STATION_REVERSE",modes.REVERSE);
+        public final double lpos;
+        public final double apos;
+        public final double wpos;
+        public final String name;
+        public final modes mode;
+        private positions(double liftPos, double armPos, double wristPos,String Name,modes direction){
+            this.lpos = liftPos;
+            this.apos = armPos;
+            this.wpos = wristPos;
+            this.name = Name;
+            this.mode = direction;
+        }
+
+    }
+    private Hashtable<DigitalInput, String> buttonMapping = new Hashtable<DigitalInput, String>();
+    
+
     private modes CurrentMode;
-    private modes currentDirection;
     private modes liftState;
+    private modes delays;
     private positions currentPosition;
+    private positions lastPosition;
 
     private String oldQuery;
     @Override
@@ -84,26 +102,44 @@ public class MasterControlProgram implements Subsystem {
         //inputs
         highGoal = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_FACE_UP);
         highGoal.addInputListener(this);
+        buttonMapping.put(highGoal,"HIGH_FORWARD");
+
         midGoal = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_FACE_LEFT);
         midGoal.addInputListener(this);
+        buttonMapping.put(midGoal,"MID_FORWARD");
+
         lowGoal = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_FACE_DOWN);
         lowGoal.addInputListener(this);
-        station = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_FACE_RIGHT);
-        station.addInputListener(this);
+        buttonMapping.put(lowGoal,"LOW_FORWARD");
+
+        stationForward = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_DPAD_RIGHT);
+        stationForward.addInputListener(this);
+        buttonMapping.put(stationForward,"STATION_FORWARD");
+
+        stationReverse = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_DPAD_LEFT);
+        stationReverse.addInputListener(this);
+        buttonMapping.put(stationReverse,"STATION_REVERSE");
+
+        groundForward = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_DPAD_UP);
+        groundReverse.addInputListener(this);
+        buttonMapping.put(groundForward,"GROUND_FORWARD");
+
+        groundReverse = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_DPAD_DOWN);
+        groundReverse.addInputListener(this);
+        buttonMapping.put(groundReverse,"GROUND_REVERSE");
+
+
         coneMode = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_LEFT_SHOULDER);
         coneMode.addInputListener(this);
         cubeMode = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_RIGHT_SHOULDER);
         cubeMode.addInputListener(this);
-        forward = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_DPAD_UP);
-        forward.addInputListener(this);
-        reverse = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_DPAD_DOWN);
-        reverse.addInputListener(this);
+
         reset = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_SELECT);
         reset.addInputListener(this);
         halt = (DigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_START); //halt button in case want to stop arm but not rest of robot.
         halt.addInputListener(this);
 
-        liftManual = (AnalogInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_LEFT_JOYSTICK_Y); //halt button in case want to stop arm but not rest of robot.
+        liftManual = (AnalogInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_LEFT_JOYSTICK_Y); 
         liftManual.addInputListener(this);
 
         armHelper = new arm();
@@ -116,26 +152,37 @@ public class MasterControlProgram implements Subsystem {
     public void resetState() {
         CurrentMode = modes.CUBE;
         currentPosition = positions.HOLDING;
-        currentDirection = modes.FORWARD;
+        lastPosition = positions.HOLDING;
         posChanged = false;
         oldQuery = "";
         haltSignal = false;
         liftResetSignal = false;
         liftState = modes.LIFT_AUTOMATIC;
         liftSpeed = 0;
+        delays = modes.FREE;
     }
 
     @Override
     public void update() {
-        if(posChanged){ //if pos has changed, update targets
-            armHelper.goToPosition(currentPosition.apos);
-            wristHelper.goToPosition(currentPosition.wpos);
-            if(liftState == modes.LIFT_AUTOMATIC){
-            liftHelper.goToPosition(currentPosition.lpos);
+        if(posChanged && delays == modes.FREE){ //if pos has changed, update targets
+            if(lastPosition.mode != currentPosition.mode &&(lastPosition.lpos > liftFlipPos || currentPosition.lpos>liftFlipPos)){
+                liftHelper.goToPosition(liftFlipPos);
+                delays = modes.HOLDING_ARM;
             }
             else{
-                liftHelper.setSpeed(liftSpeed,false);
+                armHelper.goToPosition(currentPosition.apos);
+                wristHelper.goToPosition(currentPosition.wpos);
+                if(liftState == modes.LIFT_AUTOMATIC){
+                    liftHelper.goToPosition(currentPosition.lpos);
+                }
+                else{
+                    liftHelper.setSpeed(liftSpeed,false);
+                }
             }
+        }
+        else if (posChanged){ //if flip manuver inturrpted, act as though flipping out of precaution. this might need fixing if it causes a problem.
+            liftHelper.goToPosition(liftFlipPos);
+            delays = modes.HOLDING_ARM;
         }
         if(liftResetSignal == true){
             liftHelper.resetEncoder();
@@ -147,48 +194,42 @@ public class MasterControlProgram implements Subsystem {
             liftHelper.stopMotor();
             wristHelper.stopMotor();
         }
+
+        if(delays == modes.HOLDING_ARM){
+            if(liftHelper.isReady()){ //if lift finished moving to position, move arm
+                armHelper.goToPosition(currentPosition.apos);
+                wristHelper.goToPosition(currentPosition.wpos);
+                delays = modes.HOLDING_LIFT;
+            }
+        }
+        if(delays == modes.HOLDING_LIFT && armHelper.isReady()){
+            liftHelper.goToPosition(currentPosition.lpos);
+            delays = modes.FREE;
+        }
         
     }
 
     @Override
     public void inputUpdate(Input source) { 
-        //check for mode changes
-        if(forward.getValue()){ //check if forward
-            currentDirection = modes.FORWARD;
+        //check for mode change
+        if(coneMode.getValue()){ 
+            CurrentMode = modes.CONE;
         }
-        else if(reverse.getValue()){
-            currentDirection = modes.REVERSE;
+        else if(cubeMode.getValue()){
+            CurrentMode = modes.CUBE;
         }
 
         //this next part is fun. 
         String positionQuery = "";
-        if(CurrentMode == modes.CONE && (highGoal.getValue()||midGoal.getValue())){ //first, check whether CONE or CUBE. If no goal button pressed, mode does not matter.
+        boolean goalButton = (source == highGoal || source == midGoal || source == lowGoal);
+        if(CurrentMode == modes.CONE && goalButton){ //first, check whether CONE or CUBE. If no goal button pressed, mode does not matter.
             positionQuery += "CONE_";
         }
-        else if(CurrentMode == modes.CUBE && (highGoal.getValue()||midGoal.getValue())){
+        else if(CurrentMode == modes.CUBE && goalButton){
             positionQuery += "CUBE_";
         }
-
-        if(highGoal.getValue()){ //then, check whether HIGH, MID, LOW, or STATION
-            positionQuery += "HIGH_";
-        }
-        else if (midGoal.getValue()){
-            positionQuery += "MID_";
-        }
-        else if (station.getValue()){
-            positionQuery += "STATION_";
-        }
-        else if (lowGoal.getValue()){
-            positionQuery += "GROUND_";
-        }
-
-        if(currentDirection == modes.FORWARD || highGoal.getValue()){ //then, check whether FORWARD or REVERSE. HIGH is only FORWARD
-            positionQuery += "FORWARD";
-        }
-        else{
-            positionQuery += "REVERSE";
-        }
-
+        positionQuery += (String) buttonMapping.get(source); //hopefully no error when returns null
+        
         if(reset.getValue()){
             positionQuery = "HOLDING"; //override to default position if button pressed
         }
@@ -198,6 +239,7 @@ public class MasterControlProgram implements Subsystem {
             for (positions position : positions.values()) {  //find the position with the queried name
                 if(position.name == positionQuery){
                     found = true;
+                    lastPosition = currentPosition;
                     currentPosition = position; //update current position
                 }
             }
