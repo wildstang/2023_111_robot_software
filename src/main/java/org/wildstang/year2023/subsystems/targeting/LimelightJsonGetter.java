941899ac2f3f31159e4100d2957a4837df19e74a
foxler2010 <drew@drewssite.top>
2023-01-31 19:29:07 -0600

    made double value retrieval possible
    I figured out how to use the org.json.simple library!
    I made a few things, all of which funnel into the final main "getDoubleProperty()" method
    That method is public and *automagically* fetches info about the currently
    tracked object
    for you.
    The subsystem constantly keeps parsing new up-to-date JSON and the method uses
    the most newly-parsed JSON each time so the numbers
    you get will always be current.
    If you want more details talk to me!
    -Drew M.

--- src/main/java/org/wildstang/year2023/subsystems/targeting/LimelightJsonGetter.java
+++ src/main/java/org/wildstang/year2023/subsystems/targeting/LimelightJsonGetter.java
@@ -1,16 +1,17 @@
 package org.wildstang.year2023.subsystems.targeting;
 
-import org.wildstang.framework.subsystems.Subsystem;
-import org.wildstang.framework.io.inputs.Input;
+import java.util.HashMap;
+import java.util.Map;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
-
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.wildstang.framework.io.inputs.Input;
+import org.wildstang.framework.subsystems.Subsystem;
 
-import java.util.Map;
-import java.util.HashMap;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 /**
  * Subsysytem that fetches JSON from Limelight constantly and gives access to it.
@@ -42,7 +43,7 @@ public class LimelightJsonGetter implements Subsystem {
         //just repeated here as well to decrease the amount of time we have to wait to get access to the data.
 
         //get json string
-        this.currentObjectString = NetworkTableInstance.getDefault().getTable("limelight").getEntry("json").getString(null);
+        this.latestFetch = NetworkTableInstance.getDefault().getTable("limelight").getEntry("json").getString(null);
 
         //if the string is null do not save it as the one we are parsing
         //hopefully helps to avoid most ParseExceptions
@@ -52,13 +53,9 @@ public class LimelightJsonGetter implements Subsystem {
 
         //parse the string
         try {
-
             this.currentObject = (JSONObject) parser.parse(currentObjectString);
-
         } catch (ParseException e) { //if ParseException does occur then <do something>
-
             e.printStackTrace();
-
         }
 
     }
@@ -83,19 +80,59 @@ public class LimelightJsonGetter implements Subsystem {
 
         //parse the string
         try {
-
             this.currentObject = (JSONObject) parser.parse(currentObjectString);
-
-        } catch (ParseException e) { //if ParseException does occur then <do something>
-
+        } catch (ParseException e) {
             e.printStackTrace();
-
         }
+    }
+
+    /** Get the object from inside the JSON dump that actually has the numbers.
+     * This method automatically finds out what pipeline we are using,
+     * and gets (from within the JSON dump) the JSON object that actually has the numbers.
+     * This method is used by most of the property-getter methods to clean them up,
+     * and eliminate duplicate code.
+     * @return A JSON object containing the numbers (without all the clout).
+     */
+    private JSONObject getObjectWithTheData() {
+
+        //What array of "Results {}" to look in.
+        String arrayToLookIn;
+
+        switch (this.currentPipeline) {
+            case 0: {
+                arrayToLookIn = "Fiducial";
+            }
+            case 1: {
+                arrayToLookIn = "Retro";
+            }
+            default: {
+                arrayToLookIn = "Retro";
+            }
+        }
+
+        //1 layer deep: "Results"
+        JSONObject resultsObject = (JSONObject) currentObject.get("\"Results\"");
+
+        //2 layers deep: "Fiducial", or "Retro"
+        JSONArray arrayContainingTheObjectWithTheData = (JSONArray) resultsObject.get("\"" + arrayToLookIn + "\"");
+
+        //3 layers deep: The object from index 0 within the array
+        return (JSONObject) arrayContainingTheObjectWithTheData.get(0); //JSONArray extends ArrayList so we can easily get() stuff from it
 
     }
 
-    private double getDoubleFromJSON(String... jsonPropertyPath) {
-        currentObject.g
+    /** Gets the value of a key whose value is a double
+     * WILL NOT work on keys whose value is a JSONArray
+     * @param key The key of the value you would like to retrieve
+     */
+    public double getDoubleProperty(String key) {
+
+        //continues getObjectWithTheData() from layer 3
+        JSONObject objectWithTheData = getObjectWithTheData();
+
+        //4 layers in: key
+        return (Double) objectWithTheData.get("\"" + key + "\"");
+
     }
 
     public void changePipeline(String pipelineString) {
