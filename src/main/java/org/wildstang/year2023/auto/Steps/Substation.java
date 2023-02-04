6bb08257b942fe3f0a8a20c379f1d4c27b3ddf07
R3lity <55416813+jwaters3457@users.noreply.github.com>
2023-02-04 09:55:56 -0600

    started

---
+++ src/main/java/org/wildstang/year2023/auto/Steps/Substation.java
@@ -0,0 +1,10 @@
+package org.wildstang.year2023.auto.Steps;
+
+import org.wildstang.framework.auto.AutoStep;
+import org.wildstang.framework.core.Core;
+import org.wildstang.year2023.robot.WSSubsystems;
+import org.wildstang.year2023.robot.subsystems.MasterControlProgram.MasterControlProgram;
+
+public class Substation extends AutoStep {
+    private MasterControlProgram MasterControlProgram
+}
