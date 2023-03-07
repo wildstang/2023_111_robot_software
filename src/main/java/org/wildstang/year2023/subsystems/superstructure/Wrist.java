0d18f804b6ce8318342937207eecd606e9f1e178
Stuart McCrorie <42500981+smccrorie@users.noreply.github.com>
2023-03-06 20:22:20 -0600

    Updating wrist offset

--- src/main/java/org/wildstang/year2023/subsystems/superstructure/Wrist.java
+++ src/main/java/org/wildstang/year2023/subsystems/superstructure/Wrist.java
@@ -17,7 +17,7 @@ public class Wrist {
         absEncoder.setPositionConversionFactor(360.0);
         absEncoder.setVelocityConversionFactor(360.0/60.0);
         absEncoder.setInverted(SuperConts.WRIST_ENCODER_DIRECTION);
-        absEncoder.setZeroOffset(310);//310
+        absEncoder.setZeroOffset(48.1);//310
         motor.initClosedLoop(SuperConts.WRIST_P, SuperConts.WRIST_I, SuperConts.WRIST_D, 0, absEncoder, false);
         motor.setCurrentLimit(15, 15, 0);
         
