3d90b7ccdc6f25a7cee7348fe7c8f5d1e162924d
Stuart McCrorie <42500981+smccrorie@users.noreply.github.com>
2023-04-13 18:51:15 -0500

    New offsets, and modifying intake speeds

--- src/main/java/org/wildstang/year2023/subsystems/superstructure/Wrist.java
+++ src/main/java/org/wildstang/year2023/subsystems/superstructure/Wrist.java
@@ -17,7 +17,7 @@ public class Wrist {
         absEncoder.setPositionConversionFactor(360.0);
         absEncoder.setVelocityConversionFactor(360.0/60.0);
         absEncoder.setInverted(SuperConts.WRIST_ENCODER_DIRECTION);
-        absEncoder.setZeroOffset(20.60);//310
+        absEncoder.setZeroOffset(243.11);//310
         motor.initClosedLoop(SuperConts.WRIST_P, SuperConts.WRIST_I, SuperConts.WRIST_D, 0, absEncoder, false);
         motor.setCurrentLimit(15, 15, 0);
         
